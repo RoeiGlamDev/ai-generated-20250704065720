0691de20f950e0f9d3242746ec4b6f40af731aa3
RoeiGlamDev <Roeiglam2@gmail.com>
2025-07-04 13:57:24 +0700

    Add components/Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="max-w-7xl mx-auto text-center">
+        &copy; 2023 GoldTube
+      </div>
+    </footer>
+  );
+}
